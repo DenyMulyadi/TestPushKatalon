69b17aa5de4d9699f3c48f0ba2d7f5ba3b7ee384
mulyadideny <mulyadi.deny@gmail.com>
2022-12-29 00:50:48 +0700

    testpush1

---
+++ Object Repository/New Test Object.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>2f478295-04f4-4f43-9069-91b7fbc265fd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>qwoiqpekpqe</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
